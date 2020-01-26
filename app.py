317442c8afebc94490346d1ec429d36696ad03a7
unknown <yosuke.seki@gmail.com>
2020-01-25 20:14:31 -0600

    Added postgresql

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -8,7 +10,12 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+
+if (os.environ.get('DEBUG_VALUE') == 'True'):
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+    
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'ys'
 api = Api(app)
