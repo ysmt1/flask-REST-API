80f2b38504d4f0cbf8381b2408cae53eea5fd5e2
unknown <yosuke.seki@gmail.com>
2020-01-25 19:51:02 -0600

    Fixed db reference

--- app.py
+++ app.py
@@ -6,17 +6,12 @@ from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
-from db import db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'ys'
 api = Api(app)
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
 
 jwt = JWT(app, authenticate, identity)
 
